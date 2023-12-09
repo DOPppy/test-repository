b4347a2e7e6e335c025cdb7f4e882c983758ace2
dokay <dokaypaul@gmail.com>
2023-12-09 16:11:53 +0200

    init

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+// const {google} = require('googleapis');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.listen(3000, () => console.log('app has been started'))
+
+app.post('/', (req, res) => {
+    console.log('ping was performed')
+    console.log(req.body)
+
+    if(req.body.type === 'MESSAGE') return res.json({text: `I've got an message`})
+})
+
+app.post('/min-delay', async (req, res) => {
+    console.log('ping was performed')
+    await setTimeout(()=>{}, 60000);
+    console.log(req.body)
+    if(req.body.type === 'MESSAGE') return res.json({text: `I've got an message`})
+})
